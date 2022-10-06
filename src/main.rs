ec0ff8ae15bf90907932dc859bd7409becd9df8b
akroune <crocodoctopus@gmail.com>
2022-10-06 15:33:27 -0800

    prove degree of hidden polynomial

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 struct Mod5_97(u32);
 
 fn modpow(b: u32, e: u32, p: u32) -> u32 {
-    let l = 3;
+    let l = 4;
     let mut acc = 1;
     
     for _ in 0..e/l {
@@ -45,20 +45,44 @@ impl From<i32> for Mod5_97 {
 }
 
 fn main() {
+    // Verifier's knowledge
     let x = 999;
+    let shift = 17;
+    let t = x * x - 3 * x + 2; // The target polynomial solved for x
+    let s3 = Mod5_97::from(x * x * x);
+    let s2 = Mod5_97::from(x * x);
+    let s1 = Mod5_97::from(x);
+    let s3_shift = Mod5_97::from(x * x * x) * shift;
+    let s2_shift = Mod5_97::from(x * x) * shift;
+    let s1_shift = Mod5_97::from(x) * shift;
+
+    // To prover:
+    // p(x) = x^3 - 6x^2 + 11x - 6
+    let p_c0 = 1;
+    let p_c1 = -6;
+    let p_c2 = 11;
+    let p_c3 = -6;
+    // h(x) = x - 3
+    let h_c0 = 1;
+    let h_c1 = -3;
+    let s3 = s3; // \
+    let s2 = s2; // | Prover is given these...
+    let s1 = s1; // /
+    let s3_shift = s3_shift; // \
+    let s2_shift = s2_shift; // | ...These too
+    let s1_shift = s1_shift; // /
     
-    
-    let ep0 = Mod5_97::from(x * x * x) * 1;
-    let ep1 = Mod5_97::from(x * x) * -6;
-    let ep2 = Mod5_97::from(x) * 11;
-    let ep3 = Mod5_97::from(-6);
-    let ep = ep0 + ep1 + ep2 + ep3;
-    
-    let eh0 = Mod5_97::from(x) * 1;
-    let eh1 = Mod5_97::from(-3);
-    let eh = eh0 + eh1;
-    
-    let t = x * x - 3 * x + 2;
-    
-    println!("{:?} {:?}", ep, eh * t); 
+    // Solve encrypted p and h with s factors
+    let ep = s3 * p_c0 + s2 * p_c1 + s1 * p_c2 + Mod5_97::from(p_c3);
+    let eh = s1 * h_c0 + Mod5_97::from(h_c1);
+
+    // Solve encrypted p and h with s_shift factors
+    let ep_shift = s3_shift * p_c0 + s2_shift * p_c1 + s1_shift * p_c2 + Mod5_97::from(p_c3);
+    let eh_shift = s1_shift * h_c0 + Mod5_97::from(h_c1);
+
+    // Back to verified:
+    let correct_roots = eh * t == ep;
+    let correct_form = ep * shift == ep_shift;
+    assert!(correct_roots && correct_form);
+    println!("Pass");
 }
